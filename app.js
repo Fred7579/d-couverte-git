1e69fccd244cf0bb0bbdbc343a7999e017dbec89
schneider <schneiderfrederic@yahoo.fr>
2020-11-19 16:47:49 +0100

    mise a jour formulaire

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-document.querySelector("button").addEventListener("click",function(){
-    
+document.querySelector("button").addEventListener("click", function(){
+    document.querySelector("ul").classList.toggle("hide");
 })
